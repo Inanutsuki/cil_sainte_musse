08594ebfd75ac50fc842d9e3b0e78e64e4528e43
inanutsuki <d.buzalja@gmail.com>
2021-01-28 09:58:30 +0100

    sauvegarde avant deployement

--- migrations/Version20210126080528.php
+++ migrations/Version20210126080528.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210121114816 extends AbstractMigration
+final class Version20210126080528 extends AbstractMigration
 {
     public function getDescription() : string
     {
